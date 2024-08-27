d3cafbedc2f6536bf3db8cb91d66f39da1acc288
shaheer9023 <shaheerahmad9023@gmail.com>
2024-08-27 13:19:53 +0500

    again

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,8 @@ const Blog = () => {
     return (  
         <div>  
             <h1 className="text-2xl font-bold">Blog</h1>  
-            <p>This is the blog page.</p>  
+            <p>This is the blog page.</p>
+            <h1 className="text-2xl font-bold">ME NI TO KON BAY</h1>  
         </div>  
     );  
 };  
